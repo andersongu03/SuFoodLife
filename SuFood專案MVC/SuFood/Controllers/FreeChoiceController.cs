cc9447c3afd49cc1356fbc1afa617f7dbbc4bf10
MSI\chong <aa>
2023-05-24 23:47:19 +0800

    Append "Create"、"Get"、"Delete" Methods of FreeChoicePlans, and Already have UI of freeChoicePlan, can retrieve data from the backend.

--- SuFood專案MVC/SuFood/Controllers/FreeChoiceController.cs
+++ SuFood專案MVC/SuFood/Controllers/FreeChoiceController.cs
@@ -35,5 +35,31 @@ namespace SuFood.Controllers
                 Quantity = 0
             });
         }
+
+        //GET: /FreeChoice/GetPlansAndProducts 取得方案與方案內的商品 !!有三張表的寫法
+        public Object GetPlansAndProducts()
+        {
+            return _context.ProductsOfPlans.GroupBy(p => p.PlanId).Select(group => new
+            {
+                plans = group.Select(p => new VmFreeChoicePlans
+                {
+                    PlanId = p.Plan.PlanId,
+                    PlanName = p.Plan.PlanName,
+                    PlanDescription = p.Plan.PlanDescription,
+                    PlanPrice = p.Plan.PlanPrice,
+                    PlanTotalCount = p.Plan.PlanTotalCount,
+                    PlanCanChoiceCount = p.Plan.PlanCanChoiceCount,
+                    PlanStatus = p.Plan.PlanStatus,
+                }).SingleOrDefault(),
+                Products = group.Select(p => new VmProductForFront
+                {
+                    ProductId = p.Product.ProductId,
+                    ProductName = p.Product.ProductName,
+                    Price = p.Product.Price,
+                    Img = p.Product.Img,
+                    Quantity = 0
+                })
+            });
+        }
     }
 }
